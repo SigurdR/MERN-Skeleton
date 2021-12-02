b05f34ca2280d277f21c15e75dbe9f77113b3365
CHOI <frank.choi@bycn.bouygues-construction.com>
2021-12-02 15:36:33 +0800

    Fixed PrivateRoute Component

--- client/auth/PrivateRoute.js
+++ client/auth/PrivateRoute.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import auth from './auth-helper';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={props => (
         auth.isAuthenticated() ? (
             <Component {...props} />
@@ -13,7 +13,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
             }} />
         )
     )}/>
-};
+);
 
 export default PrivateRoute;
 
